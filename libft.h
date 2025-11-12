3d1bfa83cb4545acfb8b795f3afcda40bd427a3e
Romeo Petit <rpetit@z4r5p7.42lyon.fr>
2025-11-12 12:17:38 +0100

    Feat: strmapi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rpetit <rpetit@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 09:04:09 by rpetit            #+#    #+#             */
-/*   Updated: 2025/11/12 09:24:05 by rpetit           ###   ########.fr       */
+/*   Updated: 2025/11/12 11:16:50 by rpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
